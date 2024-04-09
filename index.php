44d96afd4c505154302f190b1fe36ef0c3886478
Ahmad Mario <riotheassassin@gmail.com>
2024-04-09 11:07:23 +0700

    Footer & Header

--- index.php
+++ index.php
@@ -7,6 +7,25 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
+    <header style="background-color: #B6C4B6;">
+    <nav class="navbar container">
+            <a class="navbar-brand" href="#">Projek Akhir</a>
+            <div class="d-flex align-middle">
+                <a class="nav-link m-2" href="#">Fasilitas</a>
+                <a class="nav-link m-2" href="#">Berita</a>
+                <button class="nav-btn btn m-1" style="background-color: #163020; color:#B6C4B6"; href="#">Pendaftaran</button>
+                <button class="nav-btn btn m-1" style="background-color: #163020; color:#B6C4B6"; href="#">Login</button>
+            </div>
+    </nav>        
+    </header>
+    <!-- bodypage goes here -->
+    <footer class="fixed-bottom" style="background-color: #B6C4B6;">
+    <div class="container">
+            <p>Menara 165 Lantai 18, Jl. TB. Simatupang, 12560, Jakarta Selatan, DKI Jakarta</p>
+            <p>+62-1234-5678</p>
+            <p>projek@gmail.com</p>
+    </div>
+    </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@523distjsbootstrapbundleminjs" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>    
 </body>
 </html>
