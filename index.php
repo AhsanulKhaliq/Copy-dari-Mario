c9cb7e663b683e17eb3aeebd6010661e16e88e09
Ahmad Mario <riotheassassin@gmail.com>
2024-04-12 09:58:49 +0700

    fitur dynamic page

--- index.php
+++ index.php
@@ -9,16 +9,38 @@
 <body>
     <header style="background-color: #B6C4B6;">
     <nav class="navbar container">
-            <a class="navbar-brand" href="#">Projek Akhir</a>
+            <a class="navbar-brand" href="index.php?p=home">Projek Akhir</a>
             <div class="d-flex align-middle">
-                <a class="nav-link m-2" href="#">Fasilitas</a>
-                <a class="nav-link m-2" href="#">Berita</a>
-                <button class="nav-btn btn m-1" style="background-color: #163020; color:#B6C4B6"; href="#">Pendaftaran</button>
-                <button class="nav-btn btn m-1" style="background-color: #163020; color:#B6C4B6"; href="#">Login</button>
+                <a class="nav-link m-2" href="index.php?p=fasilitas">Fasilitas</a>
+                <a class="nav-link m-2" href="index.php?p=berita">Berita</a>
+                <a class="nav-btn btn m-1" style="background-color: #163020; color:#B6C4B6"; href="index.php?p=regist">Pendaftaran</a>
+                <a class="nav-btn btn m-1" style="background-color: #163020; color:#B6C4B6"; href="index.php?p=admin">Login</a>
             </div>
     </nav>        
     </header>
-    <!-- bodypage goes here -->
+    <main class="container">
+    <?php
+    $pages_dir = 'pages';
+    if(!empty($_GET['p'])){
+    
+    $pages = scandir($pages_dir, 0);
+    unset($pages[0], $pages[1]);
+    
+    $p = $_GET['p'];
+    if(in_array($p.'.php', $pages)){
+    
+    include($pages_dir.'/'.$p.'.php');
+    
+    } else {
+    
+    echo 'Halaman tidak ditemukan! :(';
+    
+    }
+    } else {
+        include "pages/home.php";
+    }
+    ?>
+    </main>
     <footer class="fixed-bottom" style="background-color: #B6C4B6;">
     <div class="container">
             <p>Menara 165 Lantai 18, Jl. TB. Simatupang, 12560, Jakarta Selatan, DKI Jakarta</p>
