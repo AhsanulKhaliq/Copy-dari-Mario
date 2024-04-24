d89d64921222680785fda78d5a7008fd8017cf74
Ahmad Mario <riotheassassin@gmail.com>
2024-04-24 21:33:07 +0700

    responsive content

--- pages/fasilitas.php
+++ pages/fasilitas.php
@@ -9,13 +9,13 @@
                 <li>Menyediakan lapangan olahraga sesuai minat dan bakat santri/wati.</li>
             </ul>
         </div>
-        <div class="col-6">
+        <div class="col-6 d-none d-md-block">
             <img src="https://darunnajah.com/wp-content/uploads/2017/06/IMG-20170620-WA0009.jpg" class="mt-2 rounded mx-auto img-fluid" alt="Suasana Kelas">
         </div>
     </div>
-    <br>
+    <br class="d-none d-md-block">
     <div class="row">
-        <div class="col-6">
+        <div class="col-6 d-none d-md-block">
             <img src="https://creativemedia.id/wp-content/uploads/2015/02/kursus-komputer-surabaya4.jpg" class="mt-2 rounded mx-auto img-fluid" alt="Suasana Kelas">
         </div>
         <div class="col-6">
