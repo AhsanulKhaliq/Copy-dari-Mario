566520b614c318a13a87876be3721fccee4c1cba
Ahmad Mario <riotheassassin@gmail.com>
2024-05-01 19:36:24 +0700

    tambah gambar dan page admin

--- pages/fasilitas.php
+++ pages/fasilitas.php
@@ -10,17 +10,17 @@
             </ul>
         </div>
         <div class="col-6 d-none d-md-block">
-            <img src="https://darunnajah.com/wp-content/uploads/2017/06/IMG-20170620-WA0009.jpg" class="mt-2 rounded mx-auto img-fluid" alt="Suasana Kelas">
+            <img src="assets/kelas.jpg" class="mt-2 rounded mx-auto img-fluid" alt="Suasana Kelas">
         </div>
     </div>
     <br class="d-none d-md-block">
     <div class="row">
         <div class="col-6 d-none d-md-block">
-            <img src="https://creativemedia.id/wp-content/uploads/2015/02/kursus-komputer-surabaya4.jpg" class="mt-2 rounded mx-auto img-fluid" alt="Suasana Kelas">
+            <img src="assets/komputer.jpg" class="mt-2 rounded mx-auto img-fluid" alt="Komputer">
         </div>
         <div class="col-6">
             <ul>
-                <li>Ilmu Komputer dan Informasi sudah menjadi kebutuhan pokok di masyarakat milenial sekarang, maka kami menyediakan pembeljaran Komputer dasar dengan pengenalan perangkat lunak dan keras sebagai penunjang di dunia kerja nantinya.</li>
+                <li>Ilmu Komputer dan Informasi sudah menjadi kebutuhan pokok di masyarakat milenial sekarang, maka kami menyediakan pembelajaran Komputer dasar dengan pengenalan perangkat lunak dan keras sebagai penunjang di dunia kerja nantinya.</li>
             </ul>
         </div>
     </div>
