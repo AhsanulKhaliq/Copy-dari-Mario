b6799a957ca52d9350e2993fd98f770bccaa0268
MuhSaidAbbas <baseness1470@gmail.com>
2024-05-01 07:41:42 -0700

    perbaikan placeholder

--- pages/admin.php
+++ pages/admin.php
@@ -5,8 +5,8 @@
     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="username">
   </div>
   <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label" placeholder="password">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="password">
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
