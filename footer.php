8e9c2dc3d0605e1432afb05494a66712e543a487
MuhSaidAbbas <baseness1470@gmail.com>
2024-05-03 11:31:26 -0700

    Perbaikan Footer lagi

--- footer.php
+++ footer.php
@@ -1,11 +1,12 @@
-    <footer class="absolute-bottom" style="background-color: #B6C4B6; font-family: Montserrat;">
-    <div class="row">
+    <footer class="bd-footer py-4 py-md-4 mt-4 bg-body-tertiary absolute-bottom" style="background-color: #B6C4B6; font-family: Montserrat;">
+    <div class="container py-4 py-md-5 px-4 px-md-3">
             <div class="col-md-1">
                </div>
             <div class="col-md-11">
-            <p >Menara 165 Lantai 18, Jl. TB. Simatupang, 12560, Jakarta Selatan, DKI Jakarta</p>
-            <p>+62-1234-5678</p>
-            <p>projek@gmail.com</p>
+            <img src="pngwing.png" style="width: 30px; height:30px;" class='d-block me-2'></img>
+            <span class="fs-5">Menara 165 Lantai 18, Jl. TB. Simatupang, 12560, Jakarta Selatan, DKI Jakarta</spanp>
+            <span class="fs-5"><img src="kontak.png" style="width: 30px; height:30px;" class='d-block me-2'>+62-1234-5678</img></spanp>
+            <span class="fs-5"><img src="gmail.png" style="width: 30px; height:30px;" class='d-block me-2'>projek@gmail.com</img></span>
     </div>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@523distjsbootstrapbundleminjs" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>    
