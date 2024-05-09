24a0424e28cf6c027fca5c12021ac9d1bbe12902
Ahmad Mario <riotheassassin@gmail.com>
2024-05-09 16:21:11 +0700

    Fixing assets path on footer

--- footer.php
+++ footer.php
@@ -3,10 +3,10 @@
             <div class="col-md-1">
                </div>
             <div class="col-md-11">
-            <img src="pngwing.png" style="width: 30px; height:30px;" class='d-block me-2'></img>
+            <img src="assets/pngwing.png" style="width: 30px; height:30px;" class='d-block me-2'></img>
             <span class="fs-5">Jl. Sukawati, No.09, No.7, Kec. Tanete Riattang Barat, Kab. Bone, Prov. Sulawesi Selatan</spanp>
-            <span class="fs-5"><img src="kontak.png" style="width: 30px; height:30px;" class='d-block me-2'>+62-1234-5678 (Ustadz Ahmad Marioale Sululipu) / +62-8229-694-8315 (Ustadz Muh. Said Abbas)</img></spanp>
-            <span class="fs-5"><img src="gmail.png" style="width: 30px; height:30px;" class='d-block me-2'>Darul_Taqwa@boarding_school.ac.id</img></span>
+            <span class="fs-5"><img src="assets/kontak.png" style="width: 30px; height:30px;" class='d-block me-2'>+62-1234-5678 (Ustadz Ahmad Marioale Sululipu) / +62-8229-694-8315 (Ustadz Muh. Said Abbas)</img></spanp>
+            <span class="fs-5"><img src="assets/gmail.png" style="width: 30px; height:30px;" class='d-block me-2'>Darul_Taqwa@boarding_school.ac.id</img></span>
     </div>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@523distjsbootstrapbundleminjs" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>    
