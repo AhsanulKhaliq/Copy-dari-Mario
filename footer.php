1141dadec87c50c95ac9790451ca580afdc8e777
Ahmad Mario <riotheassassin@gmail.com>
2024-05-19 09:13:07 +0700

    perbaikan footer

--- footer.php
+++ footer.php
@@ -1,10 +1,9 @@
-    <footer class="bd-footer py-4 py-md-4 mt-4 bg-body-tertiary fixed-bottom" style="background-color: #B6C4B6; font-family: Montserrat;">
+    <footer class="bd-footer py-2 bg-body-tertiary absolute-bottom" style="background-color: #B6C4B6; font-family: Montserrat;">
     <div class="container">
-            <div class="col-md-11">
-            <img src="assets/pngwing.png" style="width: 30px; height:30px;" class='d-block me-2'></img>
-            <span class="fs-5">Jl. Sukawati, No.09, No.7, Kec. Tanete Riattang Barat, Kab. Bone, Prov. Sulawesi Selatan</spanp>
-            <span class="fs-5"><img src="assets/kontak.png" style="width: 30px; height:30px;" class='d-block me-2'>+62-8229-694-8315 (Ustadz Muh. Said Abbas)</img></spanp>
-            <span class="fs-5"><img src="assets/gmail.png" style="width: 30px; height:30px;" class='d-block me-2'>Darul_Taqwa@boarding_school.ac.id</img></span>
+            <div>
+            <span class="fs-5"><img src="assets/pngwing.png" style="width: 30px; height:30px;" class='d-block me-2'>Jl. Sukawati, No.09, No.7, Kec. Tanete Riattang Barat, Kab. Bone, Prov. Sulawesi Selatan</span>
+            <span class="fs-5"><img src="assets/kontak.png" style="width: 30px; height:30px;" class='d-block me-2'>+62-8229-694-8315 (Ustadz Muh. Said Abbas)</span>
+            <span class="fs-5"><img src="assets/gmail.png" style="width: 30px; height:30px;" class='d-block me-2'>Darul_Taqwa@boarding_school.ac.id</span>
             </div>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@523distjsbootstrapbundleminjs" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>    
