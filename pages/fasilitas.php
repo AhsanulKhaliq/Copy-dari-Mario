cdd826b82702388bb5c584caf8a4fe74b593e2a9
Niella <nillaaristyauntarry@gmail.com>
2024-05-22 21:52:41 +0700

    untuk bagian home dan fasilitias udah ada beberapa fitur aku tambahin ya temanteman

--- pages/fasilitas.php
+++ pages/fasilitas.php
@@ -9,19 +9,58 @@
                 <li>Menyediakan lapangan olahraga sesuai minat dan bakat santri/wati.</li>
             </ul>
         </div>
-        <div class="col-6 d-none d-md-block">
+        <div class="col-4 d-none d-md-block">
             <img src="assets/kelas.jpg" class="mt-2 rounded mx-auto img-fluid" alt="Suasana Kelas">
         </div>
     </div>
     <br class="d-none d-md-block">
     <div class="row">
-        <div class="col-6 d-none d-md-block">
-            <img src="assets/komputer.jpg" class="mt-2 rounded mx-auto img-fluid" alt="Komputer">
+        <div class="col-4 d-none d-md-block">
+            <img src="assets/komputer.jpg" class="mt-2 rounded mx-auto img-fluid" alt="komputer">
         </div>
         <div class="col-6">
             <ul>
                 <li>Ilmu Komputer dan Informasi sudah menjadi kebutuhan pokok di masyarakat milenial sekarang, maka kami menyediakan pembelajaran Komputer dasar dengan pengenalan perangkat lunak dan keras sebagai penunjang di dunia kerja nantinya.</li>
             </ul>
         </div>
+    </div>
+
+    <div style="font-family: Montserrat">
+    <div class="row">
+        <div class="col-6">
+            <ul>
+            <li>Fasilitas kamar yang nyaman, keamanan yang terjaga dan dibimbing oleh langsung wali asrama.</li>
+
+            </ul>
+        </div>
+        <div class="col-4 d-none d-md-block">
+            <img src="assets/kamar.jpeg" class="mt-2 rounded mx-auto img-fluid" alt="Suasana kamar">
+        </div>
+    </div>
+    </div>
+<br class="d-none d-md-block">
+    <div class="row">
+        <div class="col-4 d-none d-md-block">
+            <img src="assets/lab-.jpg" class="mt-2 rounded mx-auto img-fluid" alt="lab">
+        </div>
+        <div class="col-6">
+            <ul>
+                <li>Lab Bahasa yang memudahkan kita untuk belajar berbagai macam bahasa.</li>
+            </ul>
+        </div>
+    </div>
+
+    <div style="font-family: Montserrat">
+    <div class="row">
+        <div class="col-6">
+            <ul>
+            <li>Fasilitas sains lab.</li>
+
+            </ul>
+        </div>
+        <div class="col-4 d-none d-md-block">
+            <img src="assets/sains.png" class="mt-2 rounded mx-auto img-fluid" alt="Suasana sains">
+        </div>
+    </div>
     </div>
 </div>
