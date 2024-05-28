d23a06541b08ec3a59fbf8cd5a6de5462c7c8ead
MuhSaidAbbas <baseness1470@gmail.com>
2024-05-28 08:40:27 -0700

    Mencoba membuat login admin, tapi tetap error

---
+++ class/class.Admin.php
@@ -0,0 +1,54 @@
+<?php
+
+class User extends Connection{
+
+    private $userid = 0;
+    private $email = 'a_username';
+    private $password = 'a_password';
+    private $role = '';
+    private $emp;
+
+    private $hasil = false;
+    private $message = '';
+    
+    public function __get($atribute){
+        if(property_exists($this,$atribute)){
+            return $this->$atribute;
+        }
+    }
+
+    public function __set($atribut, $value){
+        if(property_exists($this,$atribut))
+                    {$this->$atribut= $value;}
+    }
+
+
+
+    public function AddAdmin(){
+        $sql = "INSERT INTO admin (a_username, a_password, name, role)
+                values ('$this->email', '$this->password', '$this->role')";
+        $this->hasil = mysqli_query($this->connection, $sql);
+        
+        if($this->hasil)
+            $this->message ='Data berhasil ditambahkan!';
+        else
+            $this->message ='Data gagal ditambahkan!';
+        }
+        
+        public function ValidateEmail($inputemail){
+                $sql = "SELECT * FROM admin
+                        WHERE a_username = '$inputemail'";
+                $result = mysqli_query($this->connection, $sql);
+                if (mysqli_num_rows ($result) == 1){
+                    $this->hasil = true;
+                    $data = mysqli_fetch_assoc($result);
+                    $this->userid = $data['userid'];
+                    $this->password = $data['a_password'];
+                    $this->name=$data['name'];
+                    $this->email=$data['a_username'];
+                    $this->role=$data['role'];
+                    }
+        }
+        }
+
+        ?>
--- pages/admin.php
+++ pages/admin.php
@@ -1,12 +1,55 @@
+<?php
+    require_once('./class/class.Admin.php');
+    
+    if(isset($_POST['btnLogin'])){
+    
+        $email = $_POST['a_username'];
+        $password = $_POST['a_password'];
+        $objUser = new User();
+        $objUser->hasil = true;
+        $objUser->ValidateEmail($email);
+
+        if($objUser->hasil){
+        
+            $ismatch = password_verify($password, $objUser->password);
+            if($ismatch){
+                if (!isset($_SESSION)) {
+                    session_start();
+                }
+
+                $_SESSION["userid"]= $objUser->userid;
+                $_SESSION["role"]= $objUser->role;
+                $_SESSION["a_username"]= $objUser->name;
+                $_SESSION["a_password"]= $objUser->email;
+                
+                echo "<script>alert('Login sukses');</script>";
+                
+                if($objUser->role == 'admin')
+                    echo '<script>window.location = "adminRegist.php";</script>';
+                else if($objUser->role == 'index')
+                    echo '<script>window.location = "index.php";</script>';
+                
+        }
+
+            else{
+                    echo "<script>alert('Password tidak sesuai');</script>";
+                }
+            }
+            else{
+                echo "<script>alert('Email tidak terdaftar');</script>";
+            }
+        }
+
+?>
 <h1>Ahlan Wa Sahlan</h1>
 <form>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Username</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="username">
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="username" name="a_username">
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="password">
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="password" name="a_password">
   </div>
   <button type="submit" class="btn btn-primary ">Submit</button>
   <button type="cancel" class="btn btn-primary">Cancel</button>
